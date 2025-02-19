ca0a8ebfed0d03601d5e3d0e055b2299f04e247f
SamuelKrakheckeDias <161588543+SamuelKrakheckeDias@users.noreply.github.com>
2025-02-19 11:25:28 -0300

    Add files via upload

---
+++ trabalho.js
@@ -0,0 +1,3 @@
+let nm=prompt("DIgite o número do mês em que você faz aniversário:");
+let mes=["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
+console.log("Você nasceu no mês de  "+mes[nm-1]);
